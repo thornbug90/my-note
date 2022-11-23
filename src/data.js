fc8df360b295e1f74a3fb02fddca448be63d151d
thornbug90 <crazydev0409@gmail.com>
2022-11-23 21:08:45 +0300

    frontend done

--- src/data.js
+++ src/data.js
@@ -1,131 +1,54 @@
+export const types = ["React", "Interview"];
+
 const data = [
     {
-        title: "fds",
-        content: "fdsafdsa"
+        title: "How does React work?",
+        type: 0,
+        content: `React creates a virtual DOM. When state changes in a component it firstly runs a "diffing" algorithm, which identifies what has changed in the virtual DOM. The second step is reconciliation, where it updates the DOM with the results of diff.`
     },
     {
-        title: "fds",
-        content: "fdsafdsa"
+        title: "What is React?",
+        type: 0,
+        content: `React is an open-source JavaScript library created by Facebook for building complex, interactive UIs in web and mobile applications. React’s core purpose is to build UI components; it is often referred to as just the “V” (View) in an “MVC” architecture.`
     },
     {
-        title: "fds",
-        content: "fdsafdsa"
+        title: "Difference between class components and functional components?",
+        type: 0,
+        content: `<Class Components>:
+        Class-based Components uses ES6 class syntax. It can make use of the lifecycle methods.
+        Class components extend from React.Component.
+        In here you have to use this keyword to access the props and functions that you declare inside the class components.
+        Functional Components
+        Functional Components are simpler comparing to class-based functions.
+        Functional Components mainly focuses on the UI of the application, not on the behavior.
+        To be more precise these are basically render function in the class component.
+        Functional Components can have state and mimic lifecycle events using Reach Hooks`
     },
     {
-        title: "fds",
-        content: "fdsafdsa"
+        title: "What is React?",
+        type: 0,
+        content: `React is an open-source JavaScript library created by Facebook for building complex, interactive UIs in web and mobile applications. React’s core purpose is to build UI components; it is often referred to as just the “V” (View) in an “MVC” architecture.`
     },
     {
-        title: "fds",
-        content: "fdsafdsa"
+        title: "Why are you interested in this job?",
+        type: 1,
+        content: `Since pandemic, I would like to work for the people care. So I've worked some projects such as hospital infrastructure, working agency, crypto security and some more.
+        As cryptocurrencies expanded, scams increased.
+        So our company, which was a blockchain company, suffered a lot.
+        I would like to prevent illegal activities such as site phishing by misleading web technologies, and I wanted to show people true web technologies.
+        Luckily, finding a job, I found out that your company is working as a primary mission to ...
+        This is a job I really would like to work as a software developer.
+        And it's fun to work in a company where people truly believe in what they're doing!`
     },
     {
-        title: "fds",
-        content: "fdsafdsa"
+        title: "What motivates you to work at this company?",
+        type: 1,
+        content: "I'm really driven by results. I like it when I have a concrete goal to meet and actual roadmap to figure out a strong strategy for accomplishing it. At my last job, our yearly goals were very aggressive, but I worked with my manager and the rest of my team to figure out a month-by-month strategy for meeting the year-end numbers. It was a real thrill to accomplish that."
     },
     {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
-    },
-    {
-        title: "fds",
-        content: "fdsafdsa"
+        title: "What motivates you to apply for this particular role? ",
+        type: 1,
+        content: "When I was a sophomore, I really loved art and design. So I wanted to get a job for both art and science. That is the frontend postion, I think. I am happy that my creations come into direct contact with people, and I feel the greatest pride when I receive the best feedback from them. I am also motivated by working as part of a team to complete tasks and projects. I gain a great sense of job satisfaction when I know I have contributed to a team's goals or objectives."
     },
 ];
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,17 +3,40 @@ import Form from 'react-bootstrap/Form';
 import Accordion from 'react-bootstrap/Accordion';
 import Highlighter from "react-highlight-words";
 import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 
-import data from "../data";
+import data, { types } from "../data";
 import "./style.css";
 
 const Home = () => {
     const [keyword, setKeyword] = useState("");
     const [withContent, setWithContent] = useState(false);
+    const [category, setCategory] = useState([]);
 
     return (
         <div className="d-flex align-items-center justify-content-center mt-3">
-            <div className="container">
+            <div className="container" style={{ maxWidth: "600px" }}>
+                <div>
+                    {
+                        types.map((type, index) => {
+                            return (
+                                <Button
+                                    className="m-2"
+                                    variant={`${category[index] ? "" : "outline-"}light`}
+                                    key={index}
+                                    onClick={() => setCategory(prev => {
+                                        const _category = [...prev];
+                                        _category[index] = !_category[index];
+                                        return _category;
+                                    })}
+                                >
+                                    {type}
+                                </Button>
+                            );
+                        }
+                        )
+                    }
+                </div>
                 <div className="d-flex align-items-center justify-content-center mt-3">
                     <Form.Check
                         className="white d-flex align-items-center justify-content-center pe-2"
@@ -29,52 +52,47 @@ const Home = () => {
                         onChange={e => setKeyword(e.target.value)}
                     />
                 </div>
-                {data.filter(d => {
+                {data.sort((a, b) => a < b).filter(d => {
                     const keywords = keyword.split(" ");
-                    for (let i = 0; i < keywords.length; i++) {
-                        const key = keywords[i];
-                        if (withContent) {
-                            return d.title.includes(key) || d.content.includes(key);
-                        }
-                        else {
-                            return d.title.includes(key);
-                        }
+                    if (withContent) {
+                        return keywords.every((key) => {
+                            return d.title.toLowerCase().includes(key) || d.content.toLowerCase().includes(key);
+                        })
                     }
-
-                    return false;
-                }).map((datum, index) =>
-                    <Accordion className="mt-1">
-                        <Accordion.Item eventKey={index}>
-                            <Accordion.Header className="d-flex align-items-center justify-content-between">
-                                <Badge pill className="fw-normal" bg="dark">
-                                    React
-                                </Badge>
-                                <Highlighter
-                                    className="ps-3"
-                                    highlightClassName="highlight"
-                                    searchWords={keyword.split(" ")}
-                                    autoEscape={true}
-                                    textToHighlight={datum.title}
-                                />
-                            </Accordion.Header>
-                            <Accordion.Body className="text-start">
-                                {
-                                    withContent ?
-                                        <Highlighter
-                                            highlightClassName="highlight"
-                                            searchWords={keyword.split(" ")}
-                                            autoEscape={true}
-                                            textToHighlight={datum.content}
-                                        />
-                                        :
-                                        datum.content
-                                }
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
-                )}
+                    else {
+                        return keywords.every((key) => {
+                            return d.title.toLowerCase().includes(key);
+                        })
+                    }
+                }).filter(d => category.every((c) => !c) || category[d.type])
+                    .map((datum, index) =>
+                        <Accordion className="mt-1" key={index}>
+                            <Accordion.Item eventKey={index}>
+                                <Accordion.Header className="d-flex align-items-center justify-content-between">
+                                    <Badge pill className="fw-normal" bg="dark">
+                                        {types[datum.type]}
+                                    </Badge>
+                                    <Highlighter
+                                        className="ps-3"
+                                        highlightClassName="highlight"
+                                        searchWords={keyword.split(" ")}
+                                        autoEscape={true}
+                                        textToHighlight={datum.title}
+                                    />
+                                </Accordion.Header>
+                                <Accordion.Body className="text-start">
+                                    <Highlighter
+                                        highlightClassName="highlight"
+                                        searchWords={keyword.split(" ")}
+                                        autoEscape={true}
+                                        textToHighlight={datum.content}
+                                    />
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    )}
             </div>
-        </div>
+        </div >
     );
 }
 
