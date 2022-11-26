69690f24007c9e0ba2fd9fdb35c6c15c1b8bef53
thornbug90 <crazydev0409@gmail.com>
2022-11-26 16:42:36 +0300

    question added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Form from 'react-bootstrap/Form';
 import Accordion from 'react-bootstrap/Accordion';
 import Highlighter from "react-highlight-words";
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
+import FocusLock from 'react-focus-lock';
 
 import data, { types } from "../data";
 import "./style.css";
@@ -12,6 +13,25 @@ const Home = () => {
     const [keyword, setKeyword] = useState("");
     const [withContent, setWithContent] = useState(false);
     const [category, setCategory] = useState([]);
+    const inputRef = useRef();
+
+    useEffect(() => {
+        window.addEventListener("keydown", (e) => {
+            if (e.key === "Escape") { //Escape key pressed
+                e.preventDefault();
+                inputRef.current.focus();
+                setKeyword("");
+            }
+            else if (e.key === "/") { //Give input focus
+                e.preventDefault();
+                inputRef.current.focus();
+            }
+        });
+
+        return () => {
+            window.removeEventListener("keydown", null);
+        }
+    }, []);
 
     return (
         <div className="d-flex align-items-center justify-content-center mt-3">
@@ -37,60 +57,66 @@ const Home = () => {
                         )
                     }
                 </div>
-                <div className="d-flex align-items-center justify-content-center mt-3">
-                    <Form.Check
-                        className="white d-flex align-items-center justify-content-center pe-2"
-                        type="checkbox"
-                        checked={withContent}
-                        label={`content`}
-                        onChange={e => setWithContent(e.target.checked)}
-                    />
-                    <Form.Control
-                        type="text"
-                        value={keyword}
-                        placeholder="Search..."
-                        onChange={e => setKeyword(e.target.value)}
-                    />
-                </div>
-                {data.sort((a, b) => a < b).filter(d => {
-                    const keywords = keyword.split(" ");
-                    if (withContent) {
-                        return keywords.every((key) => {
-                            return d.title.toLowerCase().includes(key) || d.content.toLowerCase().includes(key);
-                        })
-                    }
-                    else {
-                        return keywords.every((key) => {
-                            return d.title.toLowerCase().includes(key);
-                        })
-                    }
-                }).filter(d => category.every((c) => !c) || category[d.type])
-                    .map((datum, index) =>
-                        <Accordion className="mt-1" key={index}>
-                            <Accordion.Item eventKey={index}>
-                                <Accordion.Header className="d-flex align-items-center justify-content-between">
-                                    <Badge pill className="fw-normal" bg="dark">
-                                        {types[datum.type]}
-                                    </Badge>
-                                    <Highlighter
-                                        className="ps-3"
-                                        highlightClassName="highlight"
-                                        searchWords={keyword.split(" ")}
-                                        autoEscape={true}
-                                        textToHighlight={datum.title}
-                                    />
-                                </Accordion.Header>
-                                <Accordion.Body className="text-start">
-                                    <Highlighter
-                                        highlightClassName="highlight"
-                                        searchWords={keyword.split(" ")}
-                                        autoEscape={true}
-                                        textToHighlight={datum.content}
-                                    />
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                    )}
+                <FocusLock>
+                    <div className="d-flex align-items-center justify-content-center mt-3">
+                        <Form.Check
+                            className="white d-flex align-items-center justify-content-center pe-2"
+                            type="checkbox"
+                            checked={withContent}
+                            label={`content`}
+                            onChange={e => setWithContent(e.target.checked)}
+                        />
+                        <Form.Control
+                            ref={inputRef}
+                            type="text"
+                            value={keyword}
+                            placeholder="Search..."
+                            onChange={e => setKeyword(e.target.value)}
+                        />
+                    </div>
+                    <Accordion className="mt-1">
+                        {data.sort((a, b) => a < b).filter(d => {
+                            const keywords = keyword.split(" ");
+                            if (withContent) {
+                                return keywords.every((key) => {
+                                    return d.title.toLowerCase().includes(key) || d.content.toLowerCase().includes(key);
+                                })
+                            }
+                            else {
+                                return keywords.every((key) => {
+                                    return d.title.toLowerCase().includes(key);
+                                })
+                            }
+                        }).filter(d => category.every((c) => !c) || category[d.type])
+                            .map((datum, index) => {
+                                return (
+                                    <Accordion.Item eventKey={index} key={index}>
+                                        <Accordion.Header className="d-flex align-items-center justify-content-between">
+                                            <Badge pill className="fw-normal" bg="dark">
+                                                {types[datum.type]}
+                                            </Badge>
+                                            <Highlighter
+                                                className="ps-3"
+                                                highlightClassName="highlight"
+                                                searchWords={keyword.split(" ")}
+                                                autoEscape={true}
+                                                textToHighlight={datum.title}
+                                            />
+                                        </Accordion.Header>
+                                        <Accordion.Body className="text-start">
+                                            <Highlighter
+                                                highlightClassName="highlight"
+                                                searchWords={keyword.split(" ")}
+                                                autoEscape={true}
+                                                textToHighlight={datum.content}
+                                            />
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                )
+                            }
+                            )}
+                    </Accordion>
+                </FocusLock>
             </div>
         </div >
     );
